30874ca6b7bc3a2bdc933357af7595f0cf2f87c8
mjodonnell5 <mjodonne@purdue.edu>
2025-05-10 16:39:20 -0400

    website

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Smooth scrolling to sections when clicking on buttons or links
-document.querySelectorAll('button').forEach(button => {
-    button.addEventListener('click', function(e) {
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
         e.preventDefault();
         const target = document.querySelector(this.getAttribute('href'));
         window.scrollTo({
